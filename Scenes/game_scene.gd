b6c9798781dca28638b7b323b38ee80aa83b75ae
Janbolat Yeleubergen <jakoyeleubergen@gmail.com>
2024-11-18 22:30:51 +0500

    prototype 0.2

--- Scenes/game_scene.gd
+++ Scenes/game_scene.gd
@@ -1,23 +1,31 @@
 extends Node
 
-@export var label: Label
+@export var score_label: Label
+@export var sps_label: Label
+@export var price_label: Label
 @export var btn: Button
 @export var upgrade_btn: Button
 
 var score = 0
-var spc = 1
+var sps = 1
+var upvol = 1
+var price = 100
 
 func _on_button_pressed():
-	score += spc
+	score += sps
 	update_label()
 
 func update_label():
-	label.text = "Score %d$" % score 
+	score_label.text = "Score %d$" % score
+	sps_label.text = "%d$ per second" % sps
+	price_label.text = "%d$" % price
 
 func _on_upgrade_pressed():
-	if score >= 100:
-		spc += 1
-		score -= 100
+	if score >= price:
+		sps += upvol
+		score -= price
+		price = price + (price * 0.25)
+		upvol += 1
 		update_label()
 	else:
 		return
