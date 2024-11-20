0353f3acb7ab544226906b02be18e7518f51dd90
Janbolat Yeleubergen <jakoyeleubergen@gmail.com>
2024-11-20 23:53:20 +0500

    prototype 0.3

--- Scenes/game_scene.gd
+++ Scenes/game_scene.gd
@@ -1,31 +1,70 @@
 extends Node
 
-@export var score_label: Label
-@export var sps_label: Label
-@export var price_label: Label
-@export var btn: Button
-@export var upgrade_btn: Button
+@export var _money: Label #total money
+@export var _mpc: Label #money_per_click
+@export var _mps: Label #money_per_sec
 
-var score = 0
-var sps = 1
-var upvol = 1
-var price = 100
+@export var _mpc_price: Label #price_money_per_click
+@export var _mps_price: Label #price_money_per_sec
 
-func _on_button_pressed():
-	score += sps
-	update_label()
+@export var _click_btn: Button #main_button
+@export var _mpc_btn: Button #buy_mpc_button
+@export var _mps_btn: Button #buy_mps_button
 
-func update_label():
-	score_label.text = "Score %d$" % score
-	sps_label.text = "%d$ per second" % sps
-	price_label.text = "%d$" % price
+var money = 0
+var mpc = 1
+var mps = 0
 
-func _on_upgrade_pressed():
-	if score >= price:
-		sps += upvol
-		score -= price
-		price = price + (price * 0.25)
-		upvol += 1
-		update_label()
-	else:
-		return
+var mpc_b_price = 10
+var coef_mpc = 1.5
+var lvl_mpc = 0
+var bp_mpc = 1
+
+var mps_b_price = 50
+var coef_mps = 2.0
+var lvl_mps = 0
+var mps_timer = 1
+var bp_mps = 1
+
+func _ready():
+	update_display()
+
+func _on_clicker_pressed():
+	money += mpc
+	update_display()
+
+
+func _on_upgrade_click_pressed():
+	var mpc_price = mpc_b_price * pow(coef_mpc, lvl_mpc)
+	if money >= mpc_price:
+		money -= mpc_price
+		lvl_mpc += 1
+		mpc += bp_mpc * lvl_mpc
+		update_display()
+
+
+func _on_upgrade_per_sec_pressed():
+	var mps_price = mps_b_price * pow(coef_mps, lvl_mps)
+	if money >= mps_price:
+		money -= mps_price
+		lvl_mps += 1
+		mps += bp_mps * lvl_mps
+		update_display()
+
+func _process(delta):
+	mps_timer += delta
+	if mps_timer >= 1.0:
+		mps_timer = 0
+		money += mps
+		update_display()
+	
+
+func update_display():
+	var mps_price = mps_b_price * pow(coef_mps, lvl_mps)
+	var mpc_price = mpc_b_price * pow(coef_mpc, lvl_mpc)
+	
+	_money.text = "%.2f$" % money
+	_mpc.text = "%.2f$/click" % mpc
+	_mps.text = "%.2f$/sec" % mps
+	_mpc_price.text = "%.2f$" % mpc_price
+	_mps_price.text = "%.2f$" % mps_price
